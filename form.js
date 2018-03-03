352476e0aae3f08ccdfaf296bd33403183983bab
halit-ozsoy-2016400141 <halitozsoy1584@gmail.com>
2018-03-03 16:07:37 +0300

    bug fixes

--- form.js
+++ form.js
@@ -2,40 +2,34 @@ import React from 'react';
 import { View } from 'react-native';
 
 export default class Form extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.inputs = [];
   }
 
-  defaultProps = {
-    inputElements: [
+  checkIfInput = element => {
+    const inputElements = this.props.inputElements ||
+    [
       {
-        names: [
-          'TextInput'
-        ], // names: 'TextInput' or name: 'TextInput' is also okay
-        submitFunctionNames: [
-          'onSubmitEditing'
-        ] // submitFunctionNames: 'onSubmitEditing' or submitFunctionName: 'onSubmitEditing' is also okay
-	focus: 'focus'
-      }
-    ]
-  }
-
-  checkIfInput = (element) => {
-    for (let i = 0; i < this.props.inputElements.length; ++i) {
+        names: ['TextInput'], // names: 'TextInput' or name: 'TextInput' is also okay
+        submitFunctionNames: ['onSubmitEditing'], // submitFunctionNames: 'onSubmitEditing' or submitFunctionName: 'onSubmitEditing' is also okay
+        focus: 'focus',
+      },
+    ];
+    for (let input of inputElements) {
       let names = [];
-      if (this.props.names) {
-        names = names.concat(this.props.names)
+      if (input.names) {
+        names = names.concat(input.names);
       }
-      if (this.props.name) {
-        names .push(this.props.name);
+      if (input.name) {
+        names.push(input.name);
       }
       if (names.includes(element.type.name)) {
-        return i;
+        return input;
       }
     }
     return false;
-  }
+  };
 
   cloneElement = (element, index, inputInfo) => {
     let functionNames = [];
@@ -45,22 +39,27 @@ export default class Form extends React.Component {
     if (inputInfo.submitFunctionName) {
       functionNames.concat(inputInfo.submitFunctionName);
     }
-    let oldEnter = null;
-    for (let i = 0; i < functionNames.length && !oldEnter; ++i) {
-      oldEnter = element.props[functionNames[i]];
+    const handleEnterBuilder = (oldEnter) => (...arg) =>
+      this.inputs[index + 1]
+        ? this.inputs[index + 1][inputInfo['focus']]()
+        : oldEnter(...arg);
+    const submitFunctions = {};
+    for (let func of functionNames) {
+      let oldFunc = element.props[func];
+      submitFunctions[func] = handleEnterBuilder(oldFunc);
     }
     const oldRef = element.props.ref;
-    const handleEnter = (...arg) =>
-      this.inputs[index + 1] ? this.inputs[index + 1][inputInfo['focus']]() : oldEnter(...arg);
     const handleRef = (ref, ...rest) => {
       this.inputs[index] = ref;
       if (oldRef) {
         oldRef(ref, ...rest);
       }
-    }
-    return React.cloneElement(element,
-      { onEnter: handleEnter, ref: handleRef );
-  }
+    };
+    return React.cloneElement(element, {
+      ...submitFunctions,
+      ref: handleRef,
+    });
+  };
 
   renderChildren(children, recursiveIndex = 0) {
     return React.Children.map(children, (child, index) => {
